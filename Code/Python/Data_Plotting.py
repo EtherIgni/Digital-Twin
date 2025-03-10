3935e82bbc0f12f1206eab09a967eab3b83861cd
EtherIgni <EtherIgni@outlook.com>
2025-03-09 22:01:51 -0400

    Here's a collection of the work I've done so far. I will start working on a higher fidelity model

--- Code/Python/Data_Plotting.py
+++ Code/Python/Data_Plotting.py
@@ -38,27 +38,19 @@ def averaging_Filter(window_size, num_passes):
 
 
 
-
-
-
 def plot_File(data,
-              time_zero,
-              filter,
               plot_labels,
               plot_colors,
               plot_title,
               plot_y_label,
               plot_save_address,
               set_y_lim):
-    #Sets the time to be zeroed to simulation start and sets it to be in mins instead of ms
-    time        = (data[-1]-time_zero)/60000
-
-
+    #Converts from ms to mins
+    time        = (data[-1])/60000
 
     #Plots Results
-    
     for idx in range(data.shape[0]-1):
-        plt.plot(time, filter(data[idx]), color=plot_colors[idx], label=plot_labels[idx])
+        plt.plot(time, data[idx], color=plot_colors[idx], label=plot_labels[idx])
     plt.legend()
     plt.title(plot_title)
     plt.ylabel(plot_y_label)
@@ -79,56 +71,86 @@ def plot_File(data,
 
 
 
-# data_file = TdmsFile.read("Data/FlowMeters.tdms")
-# data      = []
-# for group in data_file.groups():
-#     for idx, channel in enumerate(group.channels()[1:]):
-#         data.append(channel[:])
-# flow_data      = np.array(data)
+#---Mass Flow Data---
+#Sets an averaging convolution filter for the data
+data_filter=averaging_Filter(200,13)
 
-# plot_File(flow_data[:,:33985],
-#           0,
-#           lambda x: x,
-#           ["Flow in Loop 1", "Flow in Loop 2", "Flow in Loop 3"],
-#           ["red", "green", "blue"],
-#           "Mass Flow Rates",
-#           "Flow Rate (Gal/min)",
-#           "Images/Data Set 1/Mass Flow Rates No Filter.png",
-#           True)
+#Reads data from file
+data_file     = TdmsFile.read("Data/FlowMeters.tdms")
+data          = []
+for group in data_file.groups():
+    for idx, channel in enumerate(group.channels()[1:]):
+        data.append(channel[:])
+flow_data     = np.array(data)
 
+#Trims data to cut out discontinuities
+flow_data     = flow_data[:,:33985]
 
+#Zeros time to the same value across data files
+flow_data[-1] = flow_data[-1] - 284555982
 
-# plot_File(flow_data[:,:33985],
-#           0,
-#           averaging_Filter(200,13),
-#           ["Flow in Loop 1", "Flow in Loop 2", "Flow in Loop 3"],
-#           ["red", "green", "blue"],
-#           "Mass Flow Rates",
-#           "Flow Rate (Gal/min)",
-#           "Images/Data Set 1/Mass Flow Rates AV Filter.png",
-#           True)
+#Filters data
+filtered_flow_data = np.zeros(flow_data.shape)
+for i in range(flow_data.shape[0]-1):
+    filtered_flow_data[i] = data_filter(flow_data[i])
+filtered_flow_data[-1]=flow_data[-1]
 
+#Saves processed data to a text file
+filtered_flow_data.tofile("Data/Flow_Meter_Filtered.txt", sep=' ')
 
+#Plots the unfiltered data
+plot_File(flow_data,
+          ["Flow in Loop 1", "Flow in Loop 2", "Flow in Loop 3"],
+          ["red", "green", "blue"],
+          "Mass Flow Rates",
+          "Flow Rate (Gal/min)",
+          "Images/Data Set 1/Mass Flow Rates No Filter.png",
+          True)
 
-# data_file = TdmsFile.read("Data/LoopInput.tdms")
-# data      = []
-# for group in data_file.groups():
-#     for idx, channel in enumerate(group.channels()[1:]):
-#         data.append(channel[:])
-# input_data      = np.array(data)[0:len(data):2]
-
-# plot_File(input_data[:,:2701],
-#           0,
-#           lambda x: x,
-#           ["Pump 1", "Pump 2", "Valve", "Heater"],
-#           ["red", "green", "blue", "orange"],
-#           "Voltage Inputs",
-#           "Voltage (V)",
-#           "Images/Data Set 1/Voltage Inputs No Filter.png",
-#           True)
+#Plots the filtered data
+plot_File(filtered_flow_data,
+          ["Flow in Loop 1", "Flow in Loop 2", "Flow in Loop 3"],
+          ["red", "green", "blue"],
+          "Mass Flow Rates",
+          "Flow Rate (Gal/min)",
+          "Images/Data Set 1/Mass Flow Rates AV Filter.png",
+          True)
 
 
 
+#---Mass Flow Data---
+#Reads data from file
+data_file       = TdmsFile.read("Data/LoopInput.tdms")
+data            = []
+for group in data_file.groups():
+    for idx, channel in enumerate(group.channels()[1:]):
+        data.append(channel[:])
+input_data      = np.array(data)[0:len(data):2]
+
+#Trims data to cut out discontinuities
+input_data      = input_data[:,:2701]
+
+#Zeros time to the same value across data files
+input_data[-1]  = input_data[-1] - 284555982
+
+#Saves processed data to a text file
+input_data.tofile("Data/Input_Filtered.txt", sep=' ')
+
+plot_File(input_data,
+          ["Pump 1", "Pump 2", "Valve", "Heater"],
+          ["red", "green", "blue", "orange"],
+          "Voltage Inputs",
+          "Voltage (V)",
+          "Images/Data Set 1/Voltage Inputs No Filter.png",
+          True)
+
+
+
+#---Temperature Data---
+#Sets an averaging convolution filter for the data
+data_filter=averaging_Filter(200,13)
+
+#Reads data from file
 data_file = TdmsFile.read("Data/TempData.tdms")
 data      = []
 for group in data_file.groups():
@@ -136,9 +158,23 @@ for group in data_file.groups():
         data.append(channel[:35185])
 temp_data      = np.array(data)[0:len(data):2]
 
-plot_File(temp_data[[0,1,2,3,-1],1:33776],
-          0,
-          lambda x: x,
+#Trims data to cut out discontinuities
+temp_data=temp_data[:,1:33776]
+
+#Zeros time to the same value across data files
+temp_data[-1]=temp_data[-1]-284555982
+
+#Filters data
+filtered_temp_data=np.zeros(temp_data.shape)
+for i in range(temp_data.shape[0]-1):
+    filtered_temp_data[i]=data_filter(temp_data[i])
+filtered_temp_data[-1]=temp_data[-1]
+
+#Saves processed data to a text file
+filtered_temp_data.tofile("Data/Temp_Filtered.txt", sep=' ')
+
+#Plots the unfiltered data
+plot_File(temp_data[[0,1,2,3,-1],:],
           ["Loop 1-1", "Loop 1-2", "Loop 1-3", "Loop 1-4"],
           ["red", "green", "blue", "orange"],
           "Temperatures for Loop 1",
@@ -146,9 +182,8 @@ plot_File(temp_data[[0,1,2,3,-1],1:33776],
           "Images/Data Set 1/Temperatures Loop 1 No Filter.png",
           False)
 
-plot_File(temp_data[[0,1,2,3,-1],1:33776],
-          0,
-          averaging_Filter(250,13),
+#Plots the filtered data for each loop
+plot_File(filtered_temp_data[[0,1,2,3,-1],:],
           ["Loop 1-1", "Loop 1-2", "Loop 1-3", "Loop 1-4"],
           ["red", "green", "blue", "orange"],
           "Temperatures for Loop 1",
@@ -156,9 +191,7 @@ plot_File(temp_data[[0,1,2,3,-1],1:33776],
           "Images/Data Set 1/Temperatures Loop 1 AV Filter.png",
           False)
 
-plot_File(temp_data[[4,5,-1],1:33776],
-          0,
-          averaging_Filter(250,13),
+plot_File(filtered_temp_data[[4,5,-1],:],
           ["Loop 2-1", "Loop 2-2"],
           ["red", "green"],
           "Temperatures for Loop 2",
@@ -166,9 +199,7 @@ plot_File(temp_data[[4,5,-1],1:33776],
           "Images/Data Set 1/Temperatures Loop 2 AV Filter.png",
           False)
 
-plot_File(temp_data[[6,7,-1],1:33776],
-          0,
-          averaging_Filter(250,13),
+plot_File(filtered_temp_data[[6,7,-1],:],
           ["Loop 3-1", "Loop 3-2"],
           ["blue", "orange"],
           "Temperatures for Loop 3",
---
+++ Code/Python/Fitting_Procedure.py
@@ -0,0 +1,213 @@
+import numpy             as np
+import matplotlib.pyplot as plt
+from scipy.optimize  import curve_fit
+
+
+
+specific_heat_water     = 4181   #J/Kg-K
+specific_heat_exchanger = 385    #J/Kg-K
+C_to_K                  = 273.15 #K
+Gal_min_to_Kg_s         = 0.0466 #(Kg/s)/(Gal/min)
+        
+        
+        
+def calc_wrapper(power_curve,
+                 mass_flow_rate, 
+                 initial_temps,
+                 inlet_temps):
+    node_masses        = [2470,
+                          36,
+                          4,
+                          0.073,
+                          0.008,
+                          1,
+                          1,
+                          1,
+                          1,
+                          3.17,
+                          1,
+                          1]
+    coeff_e_1 = 26
+    coeff_e_2 = 25
+    heater_percent     = 0.8
+    max_power          = 2760
+    
+    
+    def calc_temps(time_steps):
+        power_curve_adjusted = power_curve*max_power
+        time_steps           = time_steps[:int(time_steps.size/7)]
+        
+        # node_masses[10] = mass_1
+        # node_masses[11] = mass_2
+
+        heat_exchanger_coeff = np.array([coeff_e_1,
+                                         coeff_e_2])
+
+        specific_heats = np.array([specific_heat_water,specific_heat_water,specific_heat_water,specific_heat_water,
+                                   specific_heat_exchanger,
+                                   specific_heat_water,specific_heat_water,specific_heat_water,specific_heat_water,
+                                   specific_heat_exchanger,
+                                   specific_heat_water,specific_heat_water])
+
+        def calc_gradient(temperatures,
+                          temp_interface,
+                          power_in,
+                          mass_flow_rates):
+            """Calculates the temperature gradient with respect to time given state parameters."""
+            gradient                 = np.zeros(len(temperatures))
+
+            loop_1_out_exchange_rate = (heat_exchanger_coeff[0])*(temperatures[2]-temperatures[4])
+            loop_2_in_exchange_rate  = (heat_exchanger_coeff[0])*(temperatures[4]-temperatures[5])
+            loop_2_out_exchange_rate = (heat_exchanger_coeff[1])*(temperatures[7]-temperatures[9])
+            loop_3_in_exchange_rate  = (heat_exchanger_coeff[1])*(temperatures[9]-temperatures[10])
+
+            gradient[0]  = (mass_flow_rates[0]*specific_heat_water)* (temperatures[3]-temperatures[0])    +(1-heater_percent)*power_in
+            gradient[1]  = (mass_flow_rates[0]*specific_heat_water)* (temperatures[0]-temperatures[1])    +    heater_percent*power_in
+            gradient[2]  = (mass_flow_rates[0]*specific_heat_water)* (temperatures[1]-temperatures[2])    -loop_1_out_exchange_rate/2
+            gradient[3]  = (mass_flow_rates[0]*specific_heat_water)* (temperatures[2]-temperatures[3])    -loop_1_out_exchange_rate/2
+
+            gradient[4]  = loop_1_out_exchange_rate-loop_2_in_exchange_rate
+
+            gradient[5]  = (mass_flow_rates[1]*specific_heat_water)* (temperatures[8]-temperatures[5])    +loop_2_in_exchange_rate/2
+            gradient[6]  = (mass_flow_rates[1]*specific_heat_water)* (temperatures[5]-temperatures[6])    +loop_2_in_exchange_rate/2
+            gradient[7]  = (mass_flow_rates[1]*specific_heat_water)* (temperatures[6]-temperatures[7])    -loop_2_out_exchange_rate/2
+            gradient[8]  = (mass_flow_rates[1]*specific_heat_water)* (temperatures[7]-temperatures[8])    -loop_2_out_exchange_rate/2
+
+            gradient[9]  = loop_2_out_exchange_rate-loop_3_in_exchange_rate
+
+            gradient[10] = (mass_flow_rates[2]*specific_heat_water)* (temp_interface-temperatures[10])     +loop_3_in_exchange_rate/2
+            gradient[11] = (mass_flow_rates[2]*specific_heat_water)* (temperatures[10]-temperatures[11])   +loop_3_in_exchange_rate/2
+
+            gradient     = gradient/(node_masses*specific_heats)
+
+            return(gradient)
+
+        #Allocates memory space
+        num_steps       = time_steps.size
+        temperatures    = np.zeros((num_steps,12))
+
+        #Defines initial conditions
+        temperatures[0] = initial_temps
+
+
+
+        for step_num in range(1, num_steps):
+            #Step forward in time
+            gradient = calc_gradient(temperatures[step_num-1],
+                                     inlet_temps[step_num-1],
+                                     power_curve_adjusted[step_num-1],
+                                     mass_flow_rates[:,step_num-1])
+            
+            #Updates values
+            temperatures[step_num] = temperatures[step_num-1] + gradient * (time_steps[step_num]-time_steps[step_num-1])
+        
+        temperatures = temperatures[:,[0,1,2,3,
+                                       7,8,
+                                       11]]
+        
+        temperatures_combined = np.concat(temperatures.T)
+        
+        return(temperatures_combined)
+    return(calc_temps)
+        
+
+input_data       = np.genfromtxt("Data/Input_Filtered.txt",      delimiter=" ").reshape((5,-1))
+flow_data        = np.genfromtxt("Data/Flow_Meter_Filtered.txt", delimiter=" ").reshape((4,-1))
+temperature_data = np.genfromtxt("Data/Temp_Filtered.txt",       delimiter=" ").reshape((9,-1))
+
+power_curve  = np.interp(temperature_data[-1],input_data[-1],input_data[3])
+flow_curve_1 = np.interp(temperature_data[-1],flow_data[-1],flow_data[0])
+flow_curve_2 = np.interp(temperature_data[-1],flow_data[-1],flow_data[1])
+flow_curve_3 = np.interp(temperature_data[-1],flow_data[-1],flow_data[2])
+
+power_curve = power_curve/10
+
+mass_flow_rates = np.array([flow_curve_1,
+                            flow_curve_2,
+                            flow_curve_3])
+mass_flow_rates = mass_flow_rates * Gal_min_to_Kg_s
+
+time = temperature_data[-1]/1000
+
+temperature_data = temperature_data[:-1] + C_to_K
+
+
+
+# for i in range(3):
+#     plt.plot(time, mass_flow_rates[i], label=str(i))
+# plt.legend()
+# plt.show()
+
+# plt.plot(time, power_curve)
+# plt.show()
+
+# for i in range(8):
+#     plt.plot(time, temperature_data[i], label=str(i))
+# plt.legend()
+# plt.show()
+
+
+
+initial_temps      = np.zeros(12)
+initial_temps[0]   = temperature_data[0,0]
+initial_temps[1]   = temperature_data[1,0]
+initial_temps[2:4] = temperature_data[2:4,0]
+initial_temps[4]   = np.mean(temperature_data[2:4,0])
+initial_temps[5:7] = temperature_data[2:4,0]
+initial_temps[7:9] = temperature_data[4:6,0]
+initial_temps[9]   = np.mean(temperature_data[4:8,0])
+initial_temps[10:] = temperature_data[6:8,0]
+
+temperature_calculator = calc_wrapper(power_curve,
+                                      mass_flow_rates,
+                                      initial_temps,
+                                      temperature_data[6])
+
+time_combined      = np.tile(time,7)
+temps_to_compare   = temperature_data[[0,1,2,3,4,5,7]]
+temp_data_combined = np.concat(temps_to_compare)
+# print(temp_data_combined)
+
+
+
+# best_fit_param, ___ = curve_fit(temperature_calculator,time_combined,temp_data_combined,[1,1])
+
+# print(best_fit_param)
+
+
+
+data = temperature_calculator(time_combined)
+
+data = data.reshape((-1,7),order="F")
+
+colors=["saddlebrown",
+        "red",
+        "orange",
+        "gold",
+        "blue",
+        "cyan",
+        "green"]
+node_names=["1-1",
+            "1-2",
+            "1-3",
+            "1-4",
+            "2-1",
+            "2-2",
+            "Out"]
+
+fig, axes = plt.subplots(1)
+
+for i in range(7):
+    plt.plot(time, data[:,i]-C_to_K, color=colors[i], label="Calc: "+node_names[i])
+    axes.plot(time, temps_to_compare[i]-C_to_K, color=colors[i], label="True: "+node_names[i], linestyle="dashed")
+axes.plot(time, temperature_data[6]-C_to_K, color="black", label="True: Inlet", linestyle="dotted")
+axes.legend()
+
+# for i in range(3):
+#     axes[1].plot(time, mass_flow_rates[i], color=colors[i], label="True: "+str(i), linestyle="dotted")
+# axes[1].legend()
+
+# axes[2].plot(time, power_curve, color="orange", label="True: "+str(i), linestyle="dotted")
+# axes[2].legend()
+
+plt.show()
