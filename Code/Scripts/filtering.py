d67730b0eb16eef03ead33b9244eecf2b97e617a
EtherIgni <EtherIgni@proton.me>
2025-04-12 14:23:36 -0400

    Filter Update

--- Code/Scripts/filtering.py
+++ Code/Scripts/filtering.py
@@ -7,20 +7,20 @@ data_folder_file_path = "C:/Users/DAQ-User/Documents/LabVIEW Data/3Loop/"
 window_size = 10
 num_passes  = 10
 
-def filter_data(run_number):
-    unfiltered_data = np.genfromtxt(data_folder_file_path+"Run 1/unfiltered_data_ready.txt")
+def filter_Data(run_number):
+    unfiltered_data                     = np.genfromtxt(data_folder_file_path+"Run "+str(run_number)+"/unfiltered_data_ready.txt")
     
-    filtered_data = np.zeros(unfiltered_data.shape)
-    filtered_data[:,0:5]=unfiltered_data[:,0:5]
+    filtered_data                       = np.zeros(unfiltered_data.shape)
+    avg_kernel                          = np.ones(window_size)/window_size
+    for series in range(1,13):
+        if(series in [4]):
+            filter_data[:,series]       = np.ones(unfiltered_data.shape[0])*unfiltered_data[:,series]
+        else:
+            for i in range(num_passes):
+                filtered_data[:,series] = convolve1d(unfiltered_data[:,series],  avg_kernel)
     
-    avg_kernel          = np.ones(window_size)/window_size
-    offset          = 5
-    for series in range(8):
-        for i in range(num_passes):
-            filtered_data[:,series+offset]  = convolve1d(unfiltered_data[:,series+offset],  avg_kernel)
     
-    
-    data_frame=pd.DataFrame.from_records(filtered_data)
-    data_frame.to_csv(data_folder_file_path+"Run 1/filtered_data.csv",mode="a",header=False,index=False)
+    data_frame                          = pd.DataFrame.from_records(filtered_data)
+    data_frame.to_csv(data_folder_file_path+"Run "+str(run_number)+"/filtered_data.csv",mode="a",header=False,index=False)
 
 filter_data(1)
